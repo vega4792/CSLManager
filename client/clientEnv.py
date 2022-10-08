6999c7454226eba9eb653589ad0f5b7529aa75aa
vega <vega4792@gmail.com>
2022-10-08 15:09:59 +0900

    Update clientEnv.py

--- client/clientEnv.py
+++ client/clientEnv.py
@@ -1,6 +1,9 @@
-
+# 서버 주소와 패스워드만 확인하고 변경하세요
 serverIP = '192.168.0.10'
 serverPass = 'wjdqh'
+
+# 아래는 되도록이면 손대지 말것(localIP는 수정하지 않아도 됩니다.)
+# CSL 우분투 기준임.
 localIP = '127.0.0.1'
 localPass = 'wjdqh'
 delayTime = 5
