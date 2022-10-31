5e4227ca6c50d96d38a7391e04c6c4dddb80ecee
vega <vega4792@gmail.com>
2022-10-31 12:13:45 +0900

    run dead client

--- remoteInstall/install-remote.sh
+++ remoteInstall/install-remote.sh
@@ -14,17 +14,5 @@ sudo mv /home/ubuntu/restart.sh /etc/ubuntu/
 
 gio set /home/ubuntu/Desktop/과제제출 metadata::nautilus-icon-position 397,119
 
-if grep "sendIP.py" /etc/rc.local ; then
-  echo "done!"
-else
-  sed -i "s/exit 0//g" /etc/rc.local
-  sed -i "s/EOF//g" /etc/rc.local
-  
-  echo "sudo python3 /etc/ubuntu/sendIP.py & " >> /etc/rc.local
-  echo "exit 0" >> /etc/rc.local
-  echo "EOF" >> /etc/rc.local
-  echo "done!"
-fi
-
 cat <(crontab -l) <(echo "*/1 * * * * sudo bash /etc/ubuntu/restart.sh >> /dev/null 2>&1") | sudo crontab -
 
--- remoteInstall/restart.sh
+++ remoteInstall/restart.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+# crontab -e
+# */1 * * * * /etc/ubuntu/restart.sh >> /etc/ubuntu/restart.log
+# chmod 755 restart.sh
 
-pid=`ps -ef | grep "sendIP" | grep -v 'grep' | awk '{print $2}'`
+for i in {1..12}; do
+	pid=`ps -ef | grep "sendIP" | grep -v 'grep' | awk '{print $2}'`
 
-if [ -z $pid ]; then
-   sudo python3 /etc/ubuntu/sendIP.py &
-fi
+	if [ -z $pid ]; then
+		sudo python3 /etc/ubuntu/sendIP.py &
+	fi
+	sleep 5
+done
